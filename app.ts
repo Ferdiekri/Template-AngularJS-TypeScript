3da9e31d831c727c2092d74facfa165872859af6
Eder Ibáñez Rojo <equ_406@hotmail.com>
2019-11-04 09:07:05 +0100

    Commit Inicial.

---
+++ app.ts
@@ -0,0 +1,6 @@
+  
+let Visualizer = window["ui-router-visualizer"].Visualizer;
+
+const app = angular.module( "repaso",       //nombre de la app o módulo principal
+                            ["ui.router"]   //Inyección de módulos o librerías
+                          ); 
